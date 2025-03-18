047ba44e31ea296ab053bd1e7d7e1f2f256ca083
James-T-S <James.Shipway@student.shu.ac.uk>
2025-03-18 15:37:17 +0000

    Update AddTenantPage.Designer.cs

--- ILet4You/AddTenantPage.Designer.cs
+++ ILet4You/AddTenantPage.Designer.cs
@@ -32,9 +32,8 @@
             this.Emailtxt = new System.Windows.Forms.TextBox();
             this.PhoneNotxt = new System.Windows.Forms.TextBox();
             this.DOBtxt = new System.Windows.Forms.TextBox();
-            this.RentDatetxt = new System.Windows.Forms.TextBox();
-            this.RentAmounttxt = new System.Windows.Forms.TextBox();
-            this.label443565 = new System.Windows.Forms.Label();
+            this.DBStxt = new System.Windows.Forms.TextBox();
+            this.Namelbl = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -47,128 +46,103 @@
             // 
             // Nametxt
             // 
-            this.Nametxt.Location = new System.Drawing.Point(317, 224);
-            this.Nametxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Nametxt.Location = new System.Drawing.Point(238, 182);
+            this.Nametxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Nametxt.Name = "Nametxt";
-            this.Nametxt.Size = new System.Drawing.Size(124, 22);
+            this.Nametxt.Size = new System.Drawing.Size(222, 20);
             this.Nametxt.TabIndex = 5;
             // 
             // Emailtxt
             // 
-            this.Emailtxt.Location = new System.Drawing.Point(317, 254);
-            this.Emailtxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Emailtxt.Location = new System.Drawing.Point(238, 206);
+            this.Emailtxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Emailtxt.Name = "Emailtxt";
-            this.Emailtxt.Size = new System.Drawing.Size(124, 22);
+            this.Emailtxt.Size = new System.Drawing.Size(222, 20);
             this.Emailtxt.TabIndex = 6;
             // 
             // PhoneNotxt
             // 
-            this.Phonenumbertxt.Location = new System.Drawing.Point(317, 283);
-            this.Phonenumbertxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Phonenumbertxt.Name = "Phonenumbertxt";
-            this.Phonenumbertxt.Size = new System.Drawing.Size(124, 22);
-            this.Phonenumbertxt.TabIndex = 7;
+            this.PhoneNotxt.Location = new System.Drawing.Point(238, 230);
+            this.PhoneNotxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.PhoneNotxt.Name = "PhoneNotxt";
+            this.PhoneNotxt.Size = new System.Drawing.Size(222, 20);
+            this.PhoneNotxt.TabIndex = 7;
             // 
             // DOBtxt
             // 
-            this.DOBtxt.Location = new System.Drawing.Point(317, 313);
-            this.DOBtxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DOBtxt.Location = new System.Drawing.Point(238, 254);
+            this.DOBtxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DOBtxt.Name = "DOBtxt";
-            this.DOBtxt.Size = new System.Drawing.Size(124, 22);
+            this.DOBtxt.Size = new System.Drawing.Size(222, 20);
             this.DOBtxt.TabIndex = 8;
             // 
             // DBStxt
             // 
-            this.RentDatetxt.Location = new System.Drawing.Point(317, 342);
-            this.RentDatetxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.RentDatetxt.Name = "RentDatetxt";
-            this.RentDatetxt.Size = new System.Drawing.Size(124, 22);
-            this.RentDatetxt.TabIndex = 9;
+            this.DBStxt.Location = new System.Drawing.Point(238, 278);
+            this.DBStxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.DBStxt.Name = "DBStxt";
+            this.DBStxt.Size = new System.Drawing.Size(222, 20);
+            this.DBStxt.TabIndex = 9;
             // 
             // Namelbl
             // 
-            this.RentAmounttxt.Location = new System.Drawing.Point(317, 372);
-            this.RentAmounttxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.RentAmounttxt.Name = "RentAmounttxt";
-            this.RentAmounttxt.Size = new System.Drawing.Size(124, 22);
-            this.RentAmounttxt.TabIndex = 10;
-            // 
-            // label443565
-            // 
-            this.label443565.AutoSize = true;
-            this.label443565.Location = new System.Drawing.Point(252, 224);
-            this.label443565.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label443565.Name = "label443565";
-            this.label443565.Size = new System.Drawing.Size(44, 16);
-            this.label443565.TabIndex = 11;
-            this.label443565.Text = "Name";
+            this.Namelbl.AutoSize = true;
+            this.Namelbl.Location = new System.Drawing.Point(189, 185);
+            this.Namelbl.Name = "Namelbl";
+            this.Namelbl.Size = new System.Drawing.Size(35, 13);
+            this.Namelbl.TabIndex = 11;
+            this.Namelbl.Text = "Name";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(256, 257);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(192, 209);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.Size = new System.Drawing.Size(32, 13);
             this.label1.TabIndex = 12;
             this.label1.Text = "Email";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(335, 185);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(313, 151);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(84, 16);
+            this.label2.Size = new System.Drawing.Size(68, 13);
             this.label2.TabIndex = 13;
             this.label2.Text = "Add Tenants";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(207, 375);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(83, 16);
-            this.label3.TabIndex = 14;
-            this.label3.Text = "Rent Amount";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(207, 346);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(155, 281);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(95, 16);
+            this.label4.Size = new System.Drawing.Size(69, 13);
             this.label4.TabIndex = 15;
             this.label4.Text = "DBS Number";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(252, 316);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(189, 257);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(36, 16);
+            this.label5.Size = new System.Drawing.Size(30, 13);
             this.label5.TabIndex = 16;
             this.label5.Text = "DOB";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(207, 287);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(146, 233);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(97, 16);
+            this.label6.Size = new System.Drawing.Size(78, 13);
             this.label6.TabIndex = 17;
             this.label6.Text = "Phone Number";
             // 
             // ConfirmButton
             // 
-            this.ConfirmButton.Location = new System.Drawing.Point(336, 438);
-            this.ConfirmButton.Margin = new System.Windows.Forms.Padding(4);
+            this.ConfirmButton.Location = new System.Drawing.Point(314, 355);
             this.ConfirmButton.Name = "ConfirmButton";
-            this.ConfirmButton.Size = new System.Drawing.Size(89, 41);
+            this.ConfirmButton.Size = new System.Drawing.Size(67, 33);
             this.ConfirmButton.TabIndex = 32;
             this.ConfirmButton.Text = "Confirm";
             this.ConfirmButton.UseVisualStyleBackColor = true;
@@ -192,26 +166,25 @@
             // 
             // AddTenantPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Notestxt);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.ConfirmButton);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.label443565);
-            this.Controls.Add(this.RentAmounttxt);
-            this.Controls.Add(this.RentDatetxt);
+            this.Controls.Add(this.Namelbl);
+            this.Controls.Add(this.DBStxt);
             this.Controls.Add(this.DOBtxt);
             this.Controls.Add(this.PhoneNotxt);
             this.Controls.Add(this.Emailtxt);
             this.Controls.Add(this.Nametxt);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "AddTenantPage";
             this.Text = "AddTenantPage";
-            this.Load += new System.EventHandler(this.AddTenantPage_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -223,9 +196,8 @@
         private System.Windows.Forms.TextBox Emailtxt;
         private System.Windows.Forms.TextBox PhoneNotxt;
         private System.Windows.Forms.TextBox DOBtxt;
-        private System.Windows.Forms.TextBox RentDatetxt;
-        private System.Windows.Forms.TextBox RentAmounttxt;
-        private System.Windows.Forms.Label label443565;
+        private System.Windows.Forms.TextBox DBStxt;
+        private System.Windows.Forms.Label Namelbl;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
